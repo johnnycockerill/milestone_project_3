e6a86652d069b903d6de47cc232e6b6e8758ad0c
johnny cockerill <jt13be6r@students.codeinstitute.net>
2025-04-21 20:15:07 +0000

    create add vehicle page

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -15,7 +15,6 @@ else:
         uri = uri.replace("postgres://", "postgresql://", 1)
     app.config["SQLALCHEMY_DATABASE_URI"] = uri
 
-
 db = SQLAlchemy(app)
 
 from taskmanager import routes  # noqa
