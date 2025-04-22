b2eb9089326dda3ca65bed4c2f1ce584fa753a0a
johnny cockerill <jt13be6r@students.codeinstitute.net>
2025-04-22 19:09:30 +0000

    create models.py, create data columns for service type and vehicles with models.py

---
+++ schedule_manager/models.py
@@ -0,0 +1,26 @@
+from schedule_manager import db
+
+
+class Service(db.Model):
+    # schema for the service type model
+    id = db.Column(db.Integer, primary_key=True)
+    service_name = db.Column(db.String(30), unique=True, nullabale=False)
+    vehicles = db.relationship("Vehicle", backref="service", cascade="all, delete", lazy=True)  # noqa
+
+    def __repr__(self):
+        return self.service_name
+
+
+class Vehicle(db.Model):
+    # schema for the Vehicle model
+    id = db.Column(db.Integer, primary_key=True)
+    vehicle_reg = db.Column(db.String(8), unique=True, nullable=False)
+    vehicle_description = db.Column(db.Text, nullable=False)
+    due_date = db.Column(db.Date, nullable=False)
+    service_completed = db.Column(db.Boolean, default=False, nullable=False)
+    service_id = db.Column(db.Integer, db.ForeignKey("service.id", ondelete="CASCADE"), nullable=False)  # noqa
+
+    def __repr__(self):
+        return "#{0} - Vehicle: {1} | Urgent: {2}".format(
+            self.vehicle_reg, self.vehicle_description, self.due_date
+        )
